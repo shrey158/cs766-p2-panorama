4daffb6fc527166eda10f36ebf0b8f2d74117d4d
zhengdx04 <zhengdx04@gmail.com>
2012-10-21 00:29:52 -0500

    10/21/2012 - 12:25 AM * Before affine drift removal, the stitched image is cropped so that the left and the right edges contain the same middle column of pixels of the first image. On this image, the affine shift gives precise seam matching. * After the affine shift, the right edge of pixels are removed since they are identical to the left edge of pixels. * More informational prompts are added to main.m to remind the user where the program is at.

--- main.m
+++ main.m
@@ -39,7 +39,9 @@ for i = 1:size(filelist,1)
     imgBuff = zeros(nRows, nCols, nClrs+1);
     imgBuff(:,:,end) = 1;
     imgBuff(:,:,1:nClrs) = img;
+    disp(['Removing distortion of ',filelist{i,1},' ...']);
     imgBuff = undistort(imgBuff,f,k1,k2,1);
+    disp(['Reprojecting ',filelist{i,1},' to cylindrical coordinates ...']);
     imgBuff = cylindrical(imgBuff,f);
     wkset(i,1) = struct('imgBuff',imgBuff, ...
                         'des', zeros(1,1), ...
@@ -50,6 +52,7 @@ end
 
 %% EXTRACT THE SIFT FEATURES
 for i = 1:size(filelist,1)
+    disp(['Extracting SIFT features of ',filelist{i,1},' ...']);
     imgBuff = wkset(i).imgBuff;
     [des, locs] = sift(imgBuff(:,:,1:end-1));
     wkset(i).des = des;
@@ -59,6 +62,8 @@ end
 
 %% PAIRWISE TRANSLATION ESTIMATION
 for i = 2:size(wkset,1)
+    disp(['Estimating translation from ', ...
+        filelist{i,1},' to ',filelist{i-1,1},' ...']);
     des1 = wkset(i-1).des;
     locs1 = wkset(i-1).locs;
     des2 = wkset(i).des;
@@ -71,20 +76,27 @@ end
 T = zeros(1,2);
 imgBuff = wkset(1).imgBuff;
 for i = 2:size(wkset,1)
+    disp(['Stitching ', filelist{i,1},' ...']);
     T = T+wkset(i).t2prev;
     [imgBuff,delT] = stitch(imgBuff, wkset(i).imgBuff, T);
     T = T+delT;
 end
 
 %% REMOVE THE DRIFT OF THE PANORAMA
+disp('Removing vertical drift ...');
+[~,nCols0,~] = size(wkset(1).imgBuff);
+c1 = floor(nCols0/2);
+imgBuff = imgBuff(:,c1:end-(nCols0-c1)+1,:);
 drift = 0;
 for i = 2:size(wkset,1)
     drift = drift + wkset(i).t2prev(1);
 end
 imgBuff = affine(imgBuff, drift);
+imgBuff = imgBuff(:,1:end-1,:);
 
 %% CROPPING THE IMAGE
 % crop out black boundaries
+disp('Cropping the panorama ...');
 [nRows, nCols, ~] = size(imgBuff);
 r1 = 1;
 while max(imgBuff(r1,:,end)) == 0
@@ -103,10 +115,7 @@ while max(imgBuff(:,c2,end)) == 0
     c2 = c2-1;
 end
 wkset(i).imgBuff = crop(imgBuff,r1,c1,r2,c2);
-% crop out all rest no-scene area and cut the end images in half
-[~,nCols0,~] = size(wkset(1).imgBuff);
-c1 = floor(nCols0/2);
-c2 = nCols - c1;
+% crop out all rest no-scene area
 r1 = 1;
 while min(imgBuff(r1,c1:c2,end)) == 0
     r1 = r1+1;
@@ -122,3 +131,4 @@ imshow(imgBuff(:,:,1:end-1));
 
 %% OUTPUT
 imwrite(imgBuff(:,:,1:end-1), [filepath,outputfile], format);
+disp('Finished and output to file.');
